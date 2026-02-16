6e5fb93e4a77d320cfc87d09da9e5dd080cd5332
samtheman19 <makin.samuel@gmail.com>
2026-02-16 23:30:29 +0000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,3 @@
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open('prdv-app').then(cache => {
-      return cache.addAll([
-        './',
-        './index.html',
-        './style.css',
-        './script.js'
-      ]);
-    })
-  );
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(response => {
-      return response || fetch(e.request);
-    })
-  );
+self.addEventListener("install", e=>{
+  self.skipWaiting();
 });
